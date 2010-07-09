b9aeec1bdf2d52bb05beba1f812784d05db84a23
Charles Boyd <csboyd07@gmail.com>
2010-07-09 20:51:50 +0000

    Added a call to JNI_OnCreate, still needs work. #647

--- android/meshapp/jni/olsr_controller.c
+++ android/meshapp/jni/olsr_controller.c
@@ -6,7 +6,40 @@
 #include "olsr_cmd.h"
 #include "net_wlanlj_meshapp_OlsrNative.h"
 
+
+JavaVM *cached_jvm;
+jclass Class_C;
+jmethodID MID_C_g;
 pid_t olsrd_pid;
+
+JNIEXPORT jint JNICALL
+JNI_OnLoad(JavaVM *jvm, void *reserved) 
+{
+  {
+     JNIEnv *env;
+     jclass cls;
+     cached_jvm = jvm;  /* cache the JavaVM pointer */
+ 
+     if ((*jvm)->GetEnv(jvm, (void **)&env, JNI_VERSION_1_2)) {
+         return JNI_ERR; /* JNI version not supported */
+     }
+     cls = (*env)->FindClass(env, "C");
+     if (cls == NULL) {
+         return JNI_ERR;
+     }
+     /* Use weak global ref to allow C class to be unloaded */
+     Class_C = (*env)->NewWeakGlobalRef(env, cls);
+     if (Class_C == NULL) {
+         return JNI_ERR;
+     }
+     /* Compute and cache the method ID */
+     MID_C_g = (*env)->GetMethodID(env, cls, "g", "()V");
+     if (MID_C_g == NULL) {
+         return JNI_ERR;
+     }
+     return JNI_VERSION_1_2;
+ }
+}
 
 void olsr_start()
 {
@@ -29,7 +62,8 @@ void olsr_stop()
   kill(olsrd_pid, SIGKILL);
 }
 
-JNIEXPORT jint JNICALL Java_net_wlanlj_meshapp_OlsrNative_start
+JNIEXPORT jint JNICALL
+Java_net_wlanlj_meshapp_OlsrNative_start
 (JNIEnv *env, jobject obj)
 {
   olsr_start();
@@ -37,7 +71,8 @@ JNIEXPORT jint JNICALL Java_net_wlanlj_meshapp_OlsrNative_start
 }
 
 
-JNIEXPORT jint JNICALL Java_net_wlanlj_meshapp_OlsrNative_stop 
+JNIEXPORT jint JNICALL
+Java_net_wlanlj_meshapp_OlsrNative_stop 
 (JNIEnv *env, jobject obj)
 {
   olsr_stop();
