d5034c7f1a6a0216b563e47a6eb6ea7d0ec377a8
Charles Boyd <csboyd07@gmail.com>
2010-07-27 04:04:08 +0000

    Did not check in some new Java source files. Replaced OlsrNative.java with GuiLibTask.java.

--- android/meshapp/src/net/wlanlj/meshapp/GuiLibTask.java
+++ android/meshapp/src/net/wlanlj/meshapp/GuiLibTask.java
@@ -2,17 +2,17 @@ package net.wlanlj.meshapp;
 
 import android.util.Log;
 
-class OlsrNative {
+class GuiLibTask {
 
-    public static final String MSG_TAG = "[[MeshApp]]::[OlsrNative] -> ";
+    public static final String MSG_TAG = "MeshApp -> GuiLibTask ";
 
     static {
 	try {
 	    Log.i(MSG_TAG, "Attempting to load GUI Library...");
-	    System.loadLibrary("guilib");
+	    System.loadLibrary("libguilib");
 	}
 	catch (Exception ex) {
-	    Log.e(MSG_TAG, "GUI Library failed to load!");
+	    Log.e(MSG_TAG, "GuiLib failed to load!");
 	}
     }
 
